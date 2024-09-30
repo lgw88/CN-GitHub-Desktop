41dd8362d065ec7397e0346b74cd11be960021e8
lgw88 <76419130+lgw88@users.noreply.github.com>
2024-09-30 09:09:26 +0800

    汉化3.4.3

---
+++ setup_en.bat
@@ -0,0 +1,39 @@
+@Echo off
+set ok=0
+goto :Start
+:RestoreEN
+if exist "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app" (
+	Echo Setup GitHub Desktop %1 恢复原英文...
+	if exist "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\main.js" (
+		copy "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\main.js" "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\"
+	)
+	if exist "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\renderer.js" (
+		copy "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\renderer.js" "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\"
+	)
+	set ok=1
+	Echo 恢复完成
+)
+Exit /b
+:Start
+Echo 恢复GitHub Desktop为原版
+Echo 获取最新版: https://github.com/lgw88/CN-GitHub-Desktop
+Call :RestoreEN	app-3.0.8
+Call :RestoreEN	app-3.3.18
+Call :RestoreEN	app-3.3.19-beta1
+Call :RestoreEN	app-3.3.19-beta2
+Call :RestoreEN	app-3.4.0
+Call :RestoreEN	app-3.4.1-beta1
+Call :RestoreEN	app-3.4.1
+Call :RestoreEN	app-3.4.2-beta1
+Call :RestoreEN	app-3.4.2-beta2
+Call :RestoreEN	app-3.4.2-beta3
+Call :RestoreEN	app-3.4.2-beta4
+Call :RestoreEN	app-3.4.3-beta1
+Call :RestoreEN	app-3.4.3-beta2
+Call :RestoreEN	app-3.4.2
+Call :RestoreEN	app-3.4.3
+Call :RestoreEN	app-3.4.3-beta3
+Echo.
+if %ok%==0 Echo 没有找到安装已知的GitHub桌面版本
+Echo 按任意键结束
+Pause >nul
---
+++ setup_tw.bat
@@ -0,0 +1,41 @@
+@Echo off
+set ok=0
+goto :Start
+:RestoreCN
+if exist "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app" (
+	Echo Setup GitHub Desktop %1 中文...
+	Md "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\"
+	if not exist "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\main.js" (
+		copy "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\main.js" "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\"
+	)
+	if not exist "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\renderer.js" (
+		copy "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\renderer.js" "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app\en\"
+	)
+	copy "%~dp0tw\%1\resources\app\*.js" "%USERPROFILE%\AppData\Local\GitHubDesktop\%1\resources\app"
+	set ok=1
+	Echo 完成
+)
+Exit /b
+:Start
+Echo ?化GitHub Desktop
+Echo ?取最新版: https://github.com/lgw88/CN-GitHub-Desktop
+Call :RestoreCN	app-3.0.8
+Call :RestoreCN	app-3.3.18
+Call :RestoreCN	app-3.3.19-beta1
+Call :RestoreCN	app-3.3.19-beta2
+Call :RestoreCN	app-3.4.0
+Call :RestoreCN	app-3.4.1-beta1
+Call :RestoreCN	app-3.4.1
+Call :RestoreCN	app-3.4.2-beta1
+Call :RestoreCN	app-3.4.2-beta2
+Call :RestoreCN	app-3.4.2-beta3
+Call :RestoreCN	app-3.4.2-beta4
+Call :RestoreCN	app-3.4.3-beta1
+Call :RestoreCN	app-3.4.3-beta2
+Call :RestoreCN	app-3.4.2
+Call :RestoreCN	app-3.4.3
+Call :RestoreCN	app-3.4.3-beta3
+Echo.
+if %ok%==0 Echo ?有找到安?GitHub已知的版本, 可以把相似的文件??本人?理
+Echo 按任意??束
+Pause >nul
