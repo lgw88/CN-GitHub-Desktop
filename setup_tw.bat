34dd78e8bf8ebe7ab2e8e0ccd18c7c4d7b930b47
lgw88 <luguowen@foxmail.com>
2025-02-13 06:32:17 +0800

    汉化3.4.17-beta2

--- setup_tw.bat
+++ setup_tw.bat
@@ -20,6 +20,7 @@ Exit /b
 Echo 漢化GitHub Desktop
 Echo 獲取最新版: https://github.com/lgw88/CN-GitHub-Desktop
 
+Call :RestoreCN	app-3.4.17-beta2
 Call :RestoreCN	app-3.4.17-beta1
 Call :RestoreCN	app-3.4.16
 Call :RestoreCN	app-3.4.16-beta1
